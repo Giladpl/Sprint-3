562b8bc9f57c30316948aab1b1ea03ab617e595c
Gilad Peltz <gilad.peltz@gmail.com>
2021-02-25 18:46:23 +0200

    whatever

--- js/apps/mail/cmps/email-side-menu.cmp.js
+++ js/apps/mail/cmps/email-side-menu.cmp.js
@@ -1,16 +1,14 @@
 export default {
 	template: `
             <section class="email-side-menu">
-							<button class="btn-side-menu">Compose</button>
+							<button @click="composeMail" class="btn-side-menu">Compose</button>
 							<button class="btn-side-menu" @click="onInbox"><i class="fas fa-inbox"></i>Inbox</button>
 							<button class="btn-side-menu"><i class="fas fa-star"></i>Starred</button>
 							<button class="btn-side-menu" @click="onSent"><i class="fas fa-share-square"></i>Sent Mail</button>
 							<button class="btn-side-menu"><i class="fab fa-firstdraft"></i>Drafts</button>
             </section>`,
 	data() {
-		return {
-			
-		};
+		return {};
 	},
 	methods: {
 		onInbox() {
@@ -18,7 +16,10 @@ export default {
 		},
 		onSent() {
 			this.$emit('onSent', 'sent');
-		}
+		},
+		composeMail() {
+			this.$emit('openCompose');
+		},
 	},
 	computed: {},
 };
