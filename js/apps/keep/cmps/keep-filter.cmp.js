c17ed9194b2d8d4e711bd35e7248bf4d398e7697
zina <zinam84@gmail.com>
2021-02-25 10:14:46 +0200

    filter

---
+++ js/apps/keep/cmps/keep-filter.cmp.js
@@ -0,0 +1,30 @@
+export default {
+    template: `
+            <section class="keep-filter">
+                <input type="text" @input="setFilter" placeholder="Search an email" v-model="filterBy.txt">
+                <select @change="selectFilter">
+                    <option value="all">All</option>
+                    <option value="txt">text</option>
+                    <option value="img">image</option>
+                    <option value="vid">video</option>
+                    <option value="todos">todos</option>
+                </select>
+            </section>`,
+    data() {
+        return {
+            filterBy: {
+				txt: '',
+				filterType: 'all',
+			},
+        }
+    },
+    methods:{
+        setFilter() {
+			this.$emit('filtered', { ...this.filterBy });
+		},
+		selectFilter(selectedFilter) {
+			this.filterBy.filterType = selectedFilter.target.value;
+			// this.$emit('filtered', { ...this.filterBy });
+		},
+    }
+}
