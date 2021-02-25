d27cf5ddb7a4d3fadd912b7c01e23e2fd1410c47
Gilad Peltz <gilad.peltz@gmail.com>
2021-02-25 20:38:17 +0200

    added logo

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,7 +1,10 @@
 export default {
 	template: `
  <header class="app-header">
-      <h1 class="logo">AppSus</h1>
+     <div class="logo-container">
+         <h1 class="logo">AppSus</h1>
+         <img src="img/logo.svg" width="50">
+     </div>
       <nav>
           <router-link to="/">Home</router-link>
           <!-- <router-link to="/about">About</router-link> -->
